7d150744dc627d6993da23efdc919f1ecca6cccf
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-18 01:22:35 +0300

    Mission Passed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+   if (username == "admin" or username =="ADMIN") and (password == "12345" or password == "123456") :
+       return 'Access granted'
+   else :
+       return 'Access denied'
+      
+admin_login("sudo", "12345")
+admin_login("admin", "123456")
+admin_login("ADMIN", "12345")     
+
 
 def hows_the_weather(temperature):
     # your code here
