6260519c316a7481b504fcf219fe4fdb930d475a
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-18 01:28:16 +0300

    Mission Passed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -12,8 +12,16 @@ admin_login("ADMIN", "12345")
 
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+  if temperature < 40 :
+      return "It's brisk out there!"
+  elif (temperature > 40) and (temperature < 65):
+       return "It's a little chilly out there!"
+  elif temperature > 85 :
+      return "It's too dang hot out there!"
+  else:
+      return "It's perfect out there!"
+
+
 
 def fizzbuzz(num):
     # your code here
